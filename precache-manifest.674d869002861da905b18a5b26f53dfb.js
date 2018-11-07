cd791d7c13352796a2edeecc9ea8baaea48ee25a
Ryan Lafferty <rlafferty1@hotmail.com>
2018-11-06 19:39:24 -0500

    Updates

---
+++ precache-manifest.674d869002861da905b18a5b26f53dfb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b926117405ea204b455",
+    "url": "/docker-swarm-kubernetes-presentation/static/js/main.6b926117.chunk.js"
+  },
+  {
+    "revision": "419bfba75c4280f8b58b",
+    "url": "/docker-swarm-kubernetes-presentation/static/css/1.d4bb1813.chunk.css"
+  },
+  {
+    "revision": "419bfba75c4280f8b58b",
+    "url": "/docker-swarm-kubernetes-presentation/static/js/1.419bfba7.chunk.js"
+  },
+  {
+    "revision": "1b7158ad5101e9380472",
+    "url": "/docker-swarm-kubernetes-presentation/static/js/runtime~main.1b7158ad.js"
+  },
+  {
+    "revision": "aa0359ed59314810c0ed52cb26b86c45",
+    "url": "/docker-swarm-kubernetes-presentation/static/media/docker-minimal.aa0359ed.png"
+  },
+  {
+    "revision": "25cae1a849c3f17678d86aecb07fe567",
+    "url": "/docker-swarm-kubernetes-presentation/static/media/modular.25cae1a8.png"
+  },
+  {
+    "revision": "afb0fe033fcd399160a9fd8b90ae1afb",
+    "url": "/docker-swarm-kubernetes-presentation/static/media/confused.afb0fe03.jpg"
+  },
+  {
+    "revision": "a5ed9c79cd884c7c0027a12b8e63739e",
+    "url": "/docker-swarm-kubernetes-presentation/static/media/containerized-apps.a5ed9c79.png"
+  },
+  {
+    "revision": "666aefaa57a61fc6dc7432fe0a29b42d",
+    "url": "/docker-swarm-kubernetes-presentation/index.html"
+  }
+];
