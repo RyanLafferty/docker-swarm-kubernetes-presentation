88e240e29334934b8b831112ed2a3081a2b5ffdd
RyanLafferty <rlafferty1@hotmail.com>
2018-11-10 11:26:30 -0500

    Added some more resources, updated UFS slides.

--- src/slides/DockerPresentation/UnionFileSystem.js
+++ src/slides/DockerPresentation/UnionFileSystem.js
@@ -3,7 +3,7 @@ import {
   Heading, Slide, Text, Appear
 } from 'spectacle';
 
-import docker from './../../res/img/DockerPresentation/docker-minimal.png';
+import docker from './../../res/img/DockerPresentation/union-file-system-simple.jpg';
 
 
 const UnionFileSystem = (
