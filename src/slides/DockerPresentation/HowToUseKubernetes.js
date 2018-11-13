19cdb347fa7f2b2099e290fa35c00ca5c54f1d48
RyanLafferty <rlafferty1@hotmail.com>
2018-11-12 20:29:57 -0500

    Added missing content

--- src/slides/DockerPresentation/HowToUseKubernetes.js
+++ src/slides/DockerPresentation/HowToUseKubernetes.js
@@ -15,7 +15,20 @@ const HowToUseKubernetes = (
       ...
     </Text>
     <Text textColor="tertiary">
-      TODO
+      To utilize the Kubernetes Cluster, we use configuration file(s) which will define our service(s)git 
+    </Text>
+    <Text style={{ visibility: 'hidden' }}>
+      ...
+    </Text>
+    <Text textColor="tertiary">
+      In this configuration file we will define our deployment(s) which are made up of pod(s)
+      containing our container(s)
+    </Text>
+    <Text style={{ visibility: 'hidden' }}>
+      ...
+    </Text>
+    <Text textColor="tertiary">
+      We can then use the cluster manager to upload our configuration files
     </Text>
   </Slide>
 );
